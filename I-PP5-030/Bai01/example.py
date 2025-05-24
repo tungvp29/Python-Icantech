31d46bb0f97b66907185fe0416915d090d2fc8d2
J <tungvp0209@gmail.com>
2025-05-24 15:27:13 +0700

    Add example Python script and score CSV file for data analysis
    - Created example.py to demonstrate the use of pandas for data manipulation.
    - Added a sample DataFrame and included reading from a CSV file (Cars.csv).
    - Introduced score.csv containing sample hours and scores data for analysis.

---
+++ I-PP5-030/Bai01/example.py
@@ -0,0 +1,27 @@
+#pip install pandas
+import pandas as pd
+
+arr = [1, 'TungVP', 10, True]
+data = pd.Series(arr, index=['STT', 'Hoten', 'Diem', 'Gioitinh'])
+# print(data)
+
+dict = { 'STT': [1, 2, 3],
+        'ten': ['TungVP', 'Nguyen Van A', 'Nguyen Van B',],
+        'diem': [10, 8, 9],
+        'gioitinh': [True, False, True] }
+
+df = pd.DataFrame(dict)
+# print(df)
+# print('-------------------')
+# dong1 = df.loc[0]
+# print(dong1)
+# print('-------------------')
+# dong13 = df.loc[[0, 2]]
+# print(dong13)
+
+df2 = pd.DataFrame(dict, index=['Hs1', 'Hs2', 'Hs3'])
+# print(df2)
+pd.options.display.max_rows = 100
+df3 = pd.read_csv('Cars.csv')
+print(df3)
+print(df3.info())
