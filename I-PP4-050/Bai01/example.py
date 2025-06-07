89f21754ecde8a6ccd0d860822fe54f3074446c1
J <tungvp0209@gmail.com>
2025-06-07 13:46:15 +0700

    cm

---
+++ I-PP4-050/Bai01/example.py
@@ -0,0 +1,8 @@
+class Dog:                  #class
+    leg = 4                 #thuộc tính của class Dog - attribute
+    breed = ''
+
+    def show(doiTuong):
+        print('Leg: ', doiTuong.leg)
+        print('Breed: ', doiTuong.breed)
+
---
+++ I-PP4-050/Bai01/project.py
@@ -0,0 +1,48 @@
+class Student:
+    name = ''
+    scores = {}                 #{'Văn': 0, 'Toán': 0, 'Anh': 0}
+
+    def scoreAvarage(std1):
+        sum(std1.scores.values())  # Tính tổng điểm các môn
+        # return (std1.scores['Văn'] + std1.scores['Toán'] + std1.scores['Anh']) / 3
+        return sum(std1.scores.values()) / len(std1.scores)  # Tính điểm trung bình
+    def showRank(std1):
+        avgScore = std1.scoreAvarage()
+        if avgScore >= 9:
+            return 'Giỏi'
+        elif avgScore >= 6:
+            return 'Khá'
+        elif avgScore >= 4:
+            return 'Trung bình'
+        else:
+            return 'Yếu'
+    
+    def showInfoStudent(std1):
+        print('Tên học sinh: ', std1.name)
+        for key in std1.scores:
+            print(f'Điểm môn {key}: {std1.scores[key]}')
+        avgScore = std1.scoreAvarage()
+        avgRound = round(avgScore, 2)
+        print(f'Điểm trung bình: {avgRound}')
+        print('Xếp loại: ', std1.showRank())
+        print('--------------------------')
+
+std1 = Student()
+std1.name = 'Tất Hải'
+std1.scores = {'Văn': 8, 'Toán': 7, 'Anh': 9}
+std1.showInfoStudent()
+
+std2 = Student()
+std2.name = 'Bảo Quốc'
+std2.scores = {'Văn': 5, 'Toán': 6, 'Anh': 4}
+std2.showInfoStudent()
+
+std3 = Student()
+std3.name = 'Minh Anh'
+std3.scores = {'Văn': 10, 'Toán': 9, 'Anh': 8}
+std3.showInfoStudent()
+
+std4 = Student()
+std4.name = 'Gia Phát'
+std4.scores = {'Văn': 3, 'Toán': 2, 'Anh': 4}
+std4.showInfoStudent()
